a8f6214698e919ff8ac564c4356aba9f5916c44b
Joe Sustaric <joe8307@gmail.com>
2024-12-23 21:16:04 +1100

    day 2 pt 2

--- advent/day02/second.py
+++ advent/day02/second.py
@@ -30,15 +30,28 @@ def is_safe(data) -> bool:
         last_direction = _get_direction(numb_1, numb_2)
     return True
 
-def _changed_direction(last_direction, numb_1, numb_2):
+def _changed_direction(last_direction, numb_1, numb_2) -> bool:
     direction = _get_direction(numb_1, numb_2)
     return last_direction is not None and (direction != last_direction)
 
-def _get_direction(numb_1, numb_2):
+def _get_direction(numb_1, numb_2) -> int:
     if (numb_2 - numb_1) > 0:
         return INCREASING
     return DECREASING
 
-def _calculate_delta(numb_1, numb_2):
+def _calculate_delta(numb_1, numb_2) -> int:
     return abs(numb_2 - numb_1)
 
+def is_safe_pt2(data) -> bool:
+    if is_safe(data):
+        return True
+
+    result = False
+    for index in range(len(data)):
+        copy = data.copy()
+        copy.pop(index)
+
+        if is_safe(copy):
+            return True
+
+    return result
--- tests/day02/second_test.py
+++ tests/day02/second_test.py
@@ -1,6 +1,8 @@
 import advent.day02.second
 import pytest
 
+# Part One
+
 # A single element report is safe
 def test_detect_safe_due_to_one_reading():
     data = [234234]
@@ -36,15 +38,64 @@ def test_detect_safe_due_to_all_increasing_in_range():
     data = [1, 3, 6, 7, 9]
     assert True == advent.day02.second.is_safe(data)
 
-def test_second_answer():
+def test_part_1_answer():
     result = 0
 
     with open('./advent/day02/second_input.txt', 'r') as file:
         for line in file:
-            split_line_string = line.split()
-            split_line_int = [int(i) for i in split_line_string]
+            split_line_int = [int(i) for i in line.split()]
 
             if advent.day02.second.is_safe(split_line_int):
                 result += 1
 
     assert  463 == result
+
+# Part Two
+
+# 7 6 4 2 1: Safe without removing any level.
+# @pytest.mark.skip(reason="Skipping this test for now")
+def test_is_safe_no_removal():
+    data = [7, 6, 4, 2, 1]
+    assert True == advent.day02.second.is_safe_pt2(data)
+
+# 1 2 7 8 9: Unsafe regardless of which level is removed.
+def test_is_unsafe_no_matter_whats_removed():
+    data = [1, 2, 7, 8, 9]
+    assert False == advent.day02.second.is_safe_pt2(data)
+
+# 9 7 6 2 1: Unsafe regardless of which level is removed.
+def test_is_unsafe_no_matter_whats_removed_2():
+    data = [9, 7, 6, 2, 1]
+    assert False == advent.day02.second.is_safe_pt2(data)
+
+# 1 3 2 4 5: Safe by removing the second level, 3.
+def test_is_safe_by_removal():
+    data = [1, 3, 2, 4, 5]
+    assert True == advent.day02.second.is_safe_pt2(data)
+
+# 8 6 4 4 1: Safe by removing the third level, 4.
+def test_is_safe_by_removal_1():
+    data = [8, 6, 4, 4, 1]
+    assert True == advent.day02.second.is_safe_pt2(data)
+
+# 8 6 4 4 1: Safe by removing the third level, 4.
+def test_is_safe_by_removal_2():
+    data = [8, 6, 4, 4, 1]
+    assert True == advent.day02.second.is_safe_pt2(data)
+
+# 1 3 6 7 9: Safe without removing any level.
+def test_is_safe_no_removal_1():
+    data = [1, 3, 6, 7, 9]
+    assert True == advent.day02.second.is_safe_pt2(data)
+
+def test_part_2_answer():
+    result = 0
+
+    with open('./advent/day02/second_input.txt', 'r') as file:
+        for line in file:
+            split_line_int = [int(i) for i in line.split()]
+
+            if advent.day02.second.is_safe_pt2(split_line_int):
+                result += 1
+
+    assert  503 == result
